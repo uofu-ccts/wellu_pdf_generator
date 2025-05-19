5518c737c098acb3a1a08be876b499401234b1b6
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2025-05-19 11:53:04 -0700

    Reverts the last two commits that removed the pdf generator page and related class methods.
    Revert "chore: remove pdfgenerator page that is no longer needed"
    
    This reverts commit ffdca0372f7653a41f85118ae4e15595309ece6f.

--- PDFGenerator.php
+++ PDFGenerator.php
@@ -86,6 +86,33 @@ class PDFGenerator extends \ExternalModules\AbstractExternalModule {
         return $record;
     }
 
+    // Get all records for this project
+    function getRecords() {
+        $params = array(
+            'project_id' => $this->project_id,
+            'return_format' => 'json',
+            // 'fields' => 'record_id'
+          );
+        $records = json_decode(\REDCap::getData($params), true);
+        $this->list_of_records = $records;
+    }
+
+    public function getOptions() {
+        if (empty($this->list_of_records)) $this->getRecords();
+
+        $html = '';
+	    foreach ($this->list_of_records as $record) {
+
+            $record_id= $record['record_id'];
+            $name = $record['name'];
+
+	        $html .= "<a class='dropdown-item' data-record-id='$record_id' data-name='$name' href='#'>" . $record_id . "</a>";
+        }
+        $html .= "<div class='dropdown-divider'></div>";
+		$html .= "<div class='dropdown-header pdf-descriptive'>Create a record to have it appear here.</div>";
+        return $html;
+    }
+
     function savePdfFile($base64Data, $filePath) {
         // Extract the base64 part (remove the data:application/pdf;base64, prefix)
         $base64Data = preg_replace('/^data:application\/pdf;filename=generated.pdf;base64,/', '', $base64Data);
@@ -106,6 +133,21 @@ class PDFGenerator extends \ExternalModules\AbstractExternalModule {
     // TODO: Save PDF (with docid) to file field
     // TODO: Have EM triggered by survey completion 
     // TODO: Set up emailing of PDF to user after generation
+
+    function returnProcessedString($string) {
+        $length = 50;
+
+        $string = strip_tags2($string);
+        if (strlen($string) > $length) {
+            $string = substr($string,0,$length) . "...";
+        }
+        $string = preg_replace('/[\n\r]+/', " ", $string);
+        return $string;
+    }
+
+    private function includeJs($path){
+        echo "<script type='text/javascript' src={$this->getUrl($path)}></script>";
+    }
 
     public function console_log($data, $level = 'INFO') {
         $output = json_encode($data);
---
+++ pages/pdfgenerator.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Utah\PDFGenerator;
+
+$user = $module->getUser();
+$username = $user->getUsername();
+
+// Handle calls from the generator page
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // $module->console_log($_POST);
+
+    // Parse required fields
+    $pdfData    = !empty($_POST['pdfData'])     ? htmlspecialchars( $_POST['pdfData'], ENT_QUOTES) : "";
+    $record_id  = !empty($_POST['record_id'])   ? htmlspecialchars( $_POST['record_id'], ENT_QUOTES) : "";
+    $name       = !empty($_POST['name'])        ? htmlspecialchars( $_POST['name'], ENT_QUOTES) : "";
+    $action     = !empty($_POST['action'])      ? htmlspecialchars( $_POST['action'], ENT_QUOTES) : "";
+
+    if ($action == 'generate_pdf') {
+        $module->console_log("Generating PDF for record: " . $record_id . " with name: " . $name);
+        $module->console_log("Action: " . $action);
+
+        $module->console_log("PDF Data: " . $pdfData);
+
+        $pdfFilePath = __DIR__ . '/' . $record_id . '.pdf';
+
+        $response = $module->savePdfFile($pdfData, $pdfFilePath);
+        $module->console_log("PDF file saved to: " . $pdfFilePath);
+        if ($response === false) {
+            $module->console_log("Failed to save PDF file.");
+        } else {
+            $module->console_log("PDF file saved successfully.");
+        }
+        // $doc_id = $module->saveToEdocs($record_id, $doc);
+        // $module->console_log("Document ID: " . $doc_id);
+    } else {
+        $module->console_log("Unknown action: " . $action);
+    }
+}
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF Generator</title>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <style>
+        .event-list {
+            display: none;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h3>PDF Generator</h3>
+
+    <h5>Welcome, <?php echo $username; ?>!</h5>
+
+    <p>
+        This is a test EM that generates a simple PDF file.
+    </p>
+
+    <div class="pdf-table">
+        <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-primaryrc dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+                Select record to create PDF <span class="caret"></span>
+            </button>
+            <div class="dropdown-menu generate-pdf">
+                <div class="dropdown-header">
+                    Select a record below:
+                </div>
+                <div class="dropdown-divider"></div>
+                <?php echo $module->getOptions(); ?>
+            </div>
+        </div>
+    </div>
+
+    <form id="action-form" name="action" class="hidden" method="POST"></form>
+</body>
+
+<script src="<?php echo $module->getUrl('js/config.js'); ?>"></script>
+
+<script>
+    PDF.addEventHandlers();
+</script>
+
+<!-- TODO: add $_SERVER['REQUEST_METHOD']=='POST' handler, and add more PHP in this page -->
